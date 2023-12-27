ca9df548d9c96c7eb8d4ac941b16d268ba150280
Gurzuff <34373848+Gurzuff@users.noreply.github.com>
2023-12-27 17:01:32 +0200

    Types annotation

--- Data/segmentation.py
+++ Data/segmentation.py
@@ -1,21 +1,28 @@
 import os
 import cv2
+import torch
+import numpy as np
 from detectron2 import model_zoo
 from detectron2.config import get_cfg
 from detectron2.engine import DefaultPredictor
 from detectron2.data import MetadataCatalog
 from detectron2.utils.visualizer import Visualizer
+from typing import List, Union
 
-def sign_segmentation(instances, metadata, im, segment_size, prob_threshold=0.5):
+def sign_segmentation(instances: torch.Tensor,
+                      metadata: MetadataCatalog,
+                      input_image: np.ndarray,
+                      segment_size: int = 32,
+                      prob_threshold: float = 0.5) -> List[np.ndarray]:
     '''
     Segments signs on the image.
     Parameters:
     - instances: Object detection model results as instances.
     - metadata: Metadata containing information about object classes and additional details.
-    - im: Input image for segmentation.
+    - input_image: Input image (NumPy array) for segmentation.
     - prob_threshold: Probability threshold for "street_sign" class detection.
-    - segment_size: Size to which the sign segments are resized (default is 64x64).
-    Returns a list of segmented sign images.
+    - segment_size: Size to which the sign segments are resized (default is 32x32).
+    Returns a list of segmented sign images (NumPy arrays).
     '''
     # Get indexes of segments corresponding to the "human" class and exceeding the probability threshold
     sign_mask = (instances.pred_classes == metadata.thing_classes.index('street_sign'))   # 'stop_sign'
@@ -31,20 +38,20 @@ def sign_segmentation(instances, metadata, im, segment_size, prob_threshold=0.5)
             x_min, y_min, x_max, y_max = sign_segment.pred_boxes.tensor.numpy()[0]
             x_delta = (x_max - x_min) * 0.1
             y_delta = (y_max - y_min) * 0.1
-            im_seg = im[int(y_min - y_delta):int(y_max + y_delta), int(x_min - x_delta):int(x_max + x_delta)]
+            im_seg = input_image[int(y_min - y_delta):int(y_max + y_delta), int(x_min - x_delta):int(x_max + x_delta)]
             im_seg = cv2.resize(im_seg, (segment_size, segment_size))
             segmented_signs.append(im_seg)
 
     return segmented_signs
 
 # CUSTOM PARAMETERS
-# shape of segmented signs: [32, 48, 64]
+# Shape of segmented signs: [32, 48, 64]
 SIZE = 32
-# threshold of probability for segmentation traffic signs
+# Threshold of probability for segmentation traffic signs
 segment_threshold = 0.45
-# path test images folder
+# Path test images folder
 PATH_test_imgs = 'raw_images'
-# weights for detectron2 model
+# Weights for detectron2 model
 detectron2_config_file = "LVISv0.5-InstanceSegmentation/mask_rcnn_X_101_32x8d_FPN_1x.yaml"
 DEVICE = "cpu"
 
